4fefc0962afab3f7bff33b4eaac42a82add50db5
Mukola-P <proxorov.mukola@gmail.com>
2022-09-17 00:55:39 +0300

    goit-jt-hw__06__task-1

---
+++ js/task-01.js
@@ -0,0 +1,10 @@
+const NumberOfCategories = document.querySelectorAll(".item")
+console.log(`Number Of Categories: ${NumberOfCategories.length}`);
+
+const CategoryAnimals = document.querySelectorAll(".item")
+CategoryAnimals.forEach((title) => {
+   
+    console.log(`
+Category 🔵🌕 ${title.firstElementChild.textContent}
+Elemants: ${title.lastElementChild.childElementCount}`);
+});
